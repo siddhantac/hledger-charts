144cf634c8c90162fddddd3327abd2e85bd1c268
Siddhanta <siddhanta.c@gmail.com>
2024-01-05 12:43:59 +0800

    moved monthly chart generation into Charts type

--- charts.go
+++ charts.go
@@ -35,3 +35,28 @@ func (c Charts) createYearlyReport(date string) {
 	page.Render(f)
 	log.Println("generated:", filename)
 }
+
+func (c Charts) createMonthlyReport(date string) {
+	page := components.NewPage()
+	page.Layout = components.PageCenterLayout
+	page.AddCharts(
+		expensesPieChart(date),
+		expensesHorizontalBarChart(date),
+		investmentsPieChart(date),
+	)
+
+	filename := fmt.Sprintf("%s/%s.html", c.OutputDir, date)
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	page.Render(f)
+	log.Println("generated:", filename)
+}
+
+func (c Charts) generateMonthlyReports(year string) {
+	for i := 1; i <= 12; i++ {
+		month := fmt.Sprintf("%s-%0.2d", year, i)
+		c.createMonthlyReport(month)
+	}
+}
--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/go-echarts/go-echarts/v2/components"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/go-echarts/go-echarts/v2/types"
 )
@@ -19,36 +16,11 @@ func defaultTheme() opts.Initialization {
 	}
 }
 
-func createMonthlyReport(date string) {
-	page := components.NewPage()
-	page.Layout = components.PageCenterLayout
-	page.AddCharts(
-		expensesPieChart(date),
-		expensesHorizontalBarChart(date),
-		investmentsPieChart(date),
-	)
-
-	filename := fmt.Sprintf("reports/%s.html", date)
-	f, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-	page.Render(f)
-	log.Println("generated:", filename)
-}
-
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
-}
-
-func generateMonthlyReports(year string) {
-	for i := 1; i <= 12; i++ {
-		month := fmt.Sprintf("%s-%0.2d", year, i)
-		createMonthlyReport(month)
-	}
 }
 
 func main() {
@@ -64,8 +36,8 @@ func main() {
 	if generateReports {
 		c := NewCharts(outputDir)
 		c.createYearlyReport("2023")
-		// createMonthlyReport("last month")
-		// generateMonthlyReports("2023")
+		c.createMonthlyReport("last month")
+		c.generateMonthlyReports("2023")
 	}
 
 	fs := http.FileServer(http.Dir(outputDir))
